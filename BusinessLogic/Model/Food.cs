7b3700371663242c1e3c6ea1cb01fee8080b2784
Sergey Nazhestkin <9497020@mail.ru>
2021-09-15 00:38:55 +0300

    Добавлена модель "Food".

---
+++ BusinessLogic/Model/Food.cs
@@ -0,0 +1,32 @@
+﻿namespace BusinessLogic.Model
+{
+    public class Food
+    {
+        public string Name { get; }
+        public double Proteins { get; }
+        public double Fats { get; }
+        public double Carbohydrates { get; }
+        public double Calories { get; }
+        private double CaloriesOnGram => Calories / 100.0;
+        private double ProteinsOnGram => Proteins / 100.0;
+        private double FatsOnGram => Fats / 100.0;
+        private double CarbohydratesOnGram => Carbohydrates / 100.0;
+
+        public Food(string name) : this(name, 0,0,0,0) { }
+
+        public Food(string name, double proteins, double fats, double carbohydrates, double calories)
+        {
+            // todo: проверка
+            Name = name;
+            Proteins = proteins / 100.0;
+            Fats = fats / 100.0;
+            Carbohydrates = carbohydrates / 100.0;
+            Calories = calories / 100.0;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
